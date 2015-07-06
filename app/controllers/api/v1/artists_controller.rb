54d480be183839ccc219ca2ffc50ed5b4fac0109
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-07-06 11:01:42 -0400

    Add api for Artist for listen page

---
+++ app/controllers/api/v1/artists_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    class ArtistsController < ApplicationController
+      before_action :set_artist, only: [:show, :update, :destroy]
+      respond_to :json
+
+      # GET /artists.json
+      def index
+        @artists = Artist.all
+        respond_with @artists
+      end
+
+      # GET /artists/1
+      def shows
+        respond_with @artist
+      end
+
+      # POST /artists
+      def create
+      @artist = Artist.create(artist_params)
+        respond_with @artist
+      end
+
+      # PATCH/PUT /artists/1
+      def update
+        @artist.update(artist_params)
+          respond_with @artist
+      end
+
+      # DELETE /artists/1
+      def destroy
+        @artist.destroy
+        respond_to do |format|
+          format.json { head :no_content }
+        end
+      end
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_artist
+        @artist = Artist.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def artist_params
+        params[:artist]
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :artists
   resources :stations
   resources :promoters
-  # namespace :api, defaults: {format: 'json'} do
-  #   namespace :v1 do
-  #     resources :artists
-  #   end
-  # end
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :artists
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
