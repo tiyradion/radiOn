d1c013f5c5f35fe9d28a48d9ff97f825137d851d
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-07-10 08:36:39 -0400

    modify application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,25 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def station_logged_in?
-    unless Station.find_by_id(session[:user_id]) && session[:user_type] == "stations"
-      redirect_to sessions_login_path, notice: "Please login"
-    end
-  end
-
-  def promoter_logged_in?
-    unless Promoter.find_by_id(session[:user_id]) && session[:user_type] == "promoters"
-      redirect_to sessions_login_path, notice: "Please login"
-    end
-  end
-
-  def logged_in?
-    if Station.find_by_id(session[:user_id]) && session[:user_type] == "stations"
-      @station = Station.find_by_id(session[:user_id])
-    elsif Promoter.find_by_id(session[:user_id]) && session[:user_type] == "promoters"
-      @promoter = Promoter.find_by_id(session[:user_id])
-    else
-      redirect_to sessions_login_path, notice: "Please login"
-    end
-  end
+  # def station_logged_in?
+  #   unless Station.find_by_id(session[:user_id]) && session[:user_type] == "stations"
+  #     redirect_to sessions_login_path, notice: "Please login"
+  #   end
+  # end
+  #
+  # def promoter_logged_in?
+  #   unless Promoter.find_by_id(session[:user_id]) && session[:user_type] == "promoters"
+  #     redirect_to sessions_login_path, notice: "Please login"
+  #   end
+  # end
+  #
+  # def logged_in?
+  #   if Station.find_by_id(session[:user_id]) && session[:user_type] == "stations"
+  #     @station = Station.find_by_id(session[:user_id])
+  #   elsif Promoter.find_by_id(session[:user_id]) && session[:user_type] == "promoters"
+  #     @promoter = Promoter.find_by_id(session[:user_id])
+  #   else
+  #     redirect_to sessions_login_path, notice: "Please login"
+  #   end
+  # end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,14 @@ class DashboardController < ApplicationController
 
   def home
   end
+
+  def logged_in?
+    if Station.find_by_id(session[:user_id]) && session[:user_type] == "stations"
+      @station = Station.find_by_id(session[:user_id])
+    elsif Promoter.find_by_id(session[:user_id]) && session[:user_type] == "promoters"
+      @promoter = Promoter.find_by_id(session[:user_id])
+    else
+      redirect_to sessions_login_path, notice: "Please login"
+    end
+  end
 end
