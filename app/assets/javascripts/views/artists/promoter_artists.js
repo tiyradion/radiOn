ad1a4720afb11056c375e150fed0508444cfd0ba
Peter McNeary <plmcneary@icloud.com>
2015-07-10 17:23:36 -0400

    Fixed new and edit artist modals

--- app/assets/javascripts/views/artists/promoter_artists.js
+++ app/assets/javascripts/views/artists/promoter_artists.js
@@ -3,10 +3,8 @@ Radion.Views.PromoterArtists = Backbone.View.extend({
   el: '.promo-artists',
 
   events: {
-    "click .open-modal": "openModal",
-    "click .close": "closeModal",
-    "click .artist-link": "openModal",
-    "click .submit-new-artist": "ajaxUpload"
+    "click .open-modal": "newArtist",
+    "click .artist-link": "editArtist"
   },
 
   template: JST['artists/promoter_artist_list'],
@@ -22,50 +20,17 @@ Radion.Views.PromoterArtists = Backbone.View.extend({
 
   },
 
-  openModal: function() {
+  newArtist: function() {
 
-    $('#openModal').addClass('showing');
-
-    $('.modal-close, .modal-container').on('click', function () {
-      $('.modal-container').removeClass('showing');
-    });
-
-    $('.modal').click(function (e) {
-      e.stopPropagation();
-    });
+    $('.artist-modal').append(new Radion.Views.NewArtist({model: new Radion.Models.Artist()}).$el);
 
   },
 
-  closeModal: function() {
+  editArtist: function(e) {
+    var artistId = $(e.target).closest('li').data('artistId');
+    var artist = this.model.get(artistId);
 
-    $('#openModal').removeClass('showing');
-
-  },
-
-  ajaxUpload: function () {
-
-    var form = $('.new-artist-form');
-    var musicSelector = $('input[type=file]', form);
-    var file = musicSelector[0].files[0];
-
-    var formData = new FormData();
-    formData.append('artist[name]', $('.new-artist-name', form).val());
-    formData.append('artist[album_name]', $('.new-album-name', form).val());
-    formData.append('artist[song_name]', $('.new-track-name', form).val());
-    formData.append('artist[uploaded_file]', file);
-
-    $.ajax({
-      url: '/api/artists', // The URL to post to
-      type: 'POST', // The HTTP method (e.g. POST)
-      data: formData, // The data to send to the server
-      processData: false, // Disable jQuery's mangling of the data
-      contentType: false, // Prevent jQuery from adding the content-type header
-      dataType: 'json' // What we expect back from server
-    }).done(console.log('Uploaded.')).fail(function () {
-      console.log(arguments);
-      alert('Failed to upload.');
-    });
-
+    $('.artist-modal').append(new Radion.Views.NewArtist({ model: artist }).$el);
   },
 
   render: function () {
