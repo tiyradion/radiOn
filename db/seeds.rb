1bc5a42a2c0e70183285eb3ded16345ef437cae9
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-07-10 14:39:49 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,19 @@ music = ["https://s3.amazonaws.com/radion-college-radio/Best+Coast+-+The+Only+Pl
   "https://s3.amazonaws.com/radion-college-radio/Yeasayer+-+2080.mp3"]
 
 5.times do
-  Station.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: "password")
+  Station.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: "password",
+    station_name: Faker::Team.name, address_1: Faker::Address.street_address, zipcode: Faker::Address.zip,
+    city: Faker::Address.city, state: Faker::Address.state_abbr, promoter_ids: [rand(1..3), rand(4..5)],
+    phone_number: Faker::PhoneNumber.phone_number)
 end
 
-20.times do
-  Comment.create(description: Faker::Company.catch_phrase, artist_id: rand(1..10), station_id: rand(1..5))
-end
+# 20.times do
+#   Comment.create(description: Faker::Company.catch_phrase, artist_id: rand(1..10), station_id: rand(1..5))
+# end
 
 5.times do
-  Promoter.create(name: Faker::Company.name, email: Faker::Internet.email, password_digest: "password")
+  Promoter.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: "password",
+    company_name: Faker::Company.name, phone_number: Faker::PhoneNumber.phone_number)
 end
 
 10.times do
