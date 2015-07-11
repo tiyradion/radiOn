da02d1443c8fde254c6144874969348b7296330e
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-07-11 17:52:25 -0400

    Modify forms in promoter and station

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::Base
   private
   def station_logged_in?
     unless Station.find_by_id(session[:user_id]) && session[:user_type] == "stations"
-      redirect_to sessions_login_path, notice: "Please login"
+      redirect_to sessions_login_path, notice: "Station login required, please login."
     end
   end
 
   def promoter_logged_in?
     unless Promoter.find_by_id(session[:user_id]) && session[:user_type] == "promoters"
-      redirect_to sessions_login_path, notice: "Please login"
+      redirect_to sessions_login_path, notice: "Promoter login required, please login."
     end
   end
 
