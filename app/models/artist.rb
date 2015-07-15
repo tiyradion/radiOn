aff8e7e28eca116577c8c518d331924132fbfd20
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-07-15 09:48:00 -0400

    New seeds with table additionals and images

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,23 +9,24 @@ class Artist < ActiveRecord::Base
   has_attached_file :picture_upload_2
   has_attached_file :picture_upload_3
   validates_attachment_content_type :uploaded_file, :content_type => ["audio/mpeg", "audio/mp3"]
-  validates_attachment_content_type :picture_upload_1, :content_type => ["image/jpeg", "image/png"]
-  validates_attachment_content_type :picture_upload_2, :content_type => ["image/jpeg", "image/png"]
-  validates_attachment_content_type :picture_upload_3, :content_type => ["image/jpeg", "image/png"]
+  validates_attachment_content_type :picture_upload_1, :content_type => ["image/jpeg", "image/jpg", "image/png"]
+  validates_attachment_content_type :picture_upload_2, :content_type => ["image/jpeg", "image/jpg", "image/png"]
+  validates_attachment_content_type :picture_upload_3, :content_type => ["image/jpeg", "image/jpg", "image/png"]
 
-  def uploaded_file_url(url)
+  def uploaded_music_url(url)
     self.uploaded_file = URI.parse(url)
   end
 
-  def request_record(response, station_id)
-    unless response == nil || response == "" || response == "false"
-      Request.create!(requested: response, artist_id: self.id, station_id: station_id)
-    end
+  def uploaded_picture_1_url(url)
+    self.picture_upload_1 = URI.parse(url)
   end
 
-  def add_comment(comment, station_id)
-    unless comment == nil || comment == ""
-      Comment.create!(description: comment, artist_id: self.id, station_id: station_id)
-    end
+  def uploaded_picture_2_url(url)
+    self.picture_upload_2 = URI.parse(url)
   end
+
+  def uploaded_picture_3_url(url)
+    self.picture_upload_3 = URI.parse(url)
+  end
+
 end
