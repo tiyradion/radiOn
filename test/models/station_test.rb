6ddddf4045ec7f196197029006ac9e84844c0685
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-07-17 17:52:29 -0400

    add unresponded field to api/promoters/id/stations for promoter dashboard

--- test/models/station_test.rb
+++ test/models/station_test.rb
@@ -7,11 +7,11 @@ class StationTest < ActiveSupport::TestCase
   end
 
   test "artist not reviewed" do
-    assert_equal "", @station.artist_not_reviewed.count
+    assert_equal 0, @station.artists_not_reviewed.count
   end
 
   test "unresponded feedback from one promoter" do
-    assert_equal "", @station.unresponded_feedback(@promoter.id)
+    assert_equal 1, @station.unresponded_feedback(@promoter.id).count
   end
 
 end
