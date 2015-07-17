c1b5470fa75d6cc5d14f1c954fd1f1dfabdebed8
Peter McNeary <plmcneary@icloud.com>
2015-07-17 10:27:59 -0400

    Adding fields to edit/new artist form

--- app/assets/javascripts/views/promoters/new_artist.js
+++ app/assets/javascripts/views/promoters/new_artist.js
@@ -18,14 +18,30 @@ Radion.Views.NewArtist = Backbone.View.extend({
   ajaxUpload: function() {
 
     var form = $('.new-artist-form');
-    var musicSelector = $('input[type=file]', form);
-    var file = musicSelector[0].files[0];
+    var pictureSelector1 = $('.new-picture-file-1', form);
+    var pictureFile1 = pictureSelector1[0].files[0];
+    var pictureSelector2 = $('.new-picture-file-2', form);
+    var pictureFile2 = pictureSelector2[0].files[0];
+    var pictureSelector3 = $('.new-picture-file-3', form);
+    var pictureFile3 = pictureSelector3[0].files[0];
+    var pictureSelector4 = $('.new-picture-file-4', form);
+    var pictureFile4 = pictureSelector4[0].files[0];
+    var pictureSelector5 = $('.new-picture-file-5', form);
+    var pictureFile5 = pictureSelector5[0].files[0];
+    var musicSelector = $('.new-music-file', form);
+    var musicFile = musicSelector[0].files[0];
 
     var formData = new FormData();
     formData.append('artist[name]', $('.new-artist-name', form).val());
     formData.append('artist[album_name]', $('.new-album-name', form).val());
     formData.append('artist[song_name]', $('.new-track-name', form).val());
-    formData.append('artist[uploaded_file]', file);
+    formData.append('artist[bio]', $('.new-artist-bio', form).val());
+    formData.append('artist[picture_upload_1_file]', pictureFile1);
+    formData.append('artist[picture_upload_2_file]', pictureFile2);
+    formData.append('artist[picture_upload_3_file]', pictureFile3);
+    formData.append('artist[picture_upload_4_file]', pictureFile4);
+    formData.append('artist[picture_upload_5_file]', pictureFile5);
+    formData.append('artist[music_upload_file]', musicFile);
 
     $.ajax({
       url: '/api/artists', // The URL to post to
