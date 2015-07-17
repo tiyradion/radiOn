ee2ae064afbb9a0265564fdf9de2e3115e2126a9
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-07-17 15:32:17 -0400

    Update contacts array working

--- app/models/promoter.rb
+++ app/models/promoter.rb
@@ -5,7 +5,7 @@ class Promoter < ActiveRecord::Base
   has_secure_password
   has_attached_file :picture_upload
   validates_attachment_content_type :picture_upload, :content_type => ["image/jpeg", "image/jpg", "image/png"]
-  validates :name, :password, :company_name, presence: true
+  validates :name, :company_name, presence: true
   validates :email, presence: true, uniqueness: true
 
   def picture_upload_url(url)
