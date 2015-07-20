7f1edea1a83b5e73019c33421f65b6fadd7a23d2
Peter McNeary <plmcneary@icloud.com>
2015-07-20 11:07:47 -0400

    Fixed add artist

--- app/assets/javascripts/views/stations/new_ranking.js
+++ app/assets/javascripts/views/stations/new_ranking.js
@@ -1,14 +1,24 @@
 Radion.Views.NewRanking = Backbone.View.extend({
 
-  className: 'modalDialog showing',
+  className: 'modalDialog showing ranking-modal',
+
+  events: {
+    'click .ranking-artist': 'addRanking'
+  },
 
   template: JST['stations/new_ranking'],
 
   initialize: function() {
 
-    this.model.fetch();
+    this.listenTo(this.model, 'change remove add', this.render);
 
-    this.render();
+    this.refresh();
+
+  },
+
+  addRanking: function (e) {
+
+
 
   },
 
@@ -18,7 +28,15 @@ Radion.Views.NewRanking = Backbone.View.extend({
 
   },
 
+  refresh: function() {
+    this.model.fetch({silent: true}).done(this.render.bind(this)).fail(function () {
+      alert('Failed to load artists.');
+    });
+  },
+
   render: function() {
+
+    console.log(this.model);
 
     this.$el.html(this.template({
       artists: this.model.toJSON()
