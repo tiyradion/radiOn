56472e5d3cdfd3d0b4be54fc2e6073104c3e2418
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-07-21 14:04:21 -0400

    deploy secure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,8 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :url =>':s3_domain_url',
+    :s3_protocol => :https,
+    # :url =>':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename',
     :s3_credentials => {
     :bucket => "lodial-college-radio",
